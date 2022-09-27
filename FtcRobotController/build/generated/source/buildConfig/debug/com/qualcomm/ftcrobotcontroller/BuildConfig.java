8814977a8fe340b21f1df1975293705e945af063
ReedCityCoyotesTeam11940 <103961614+ReedCityCoyotesTeam11940@users.noreply.github.com>
2022-09-27 13:07:16 -0400

    Code on the DEV compiled w/o error

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-09-27T12:15:04.473-0400";
+  public static final String APP_BUILD_TIME = "2022-09-27T12:27:46.213-0400";
 }
