30f22701627aa770f0ed6f8b6883f8c05abea5d2
Liam VanScoyoc <liamvanscoyoc@gmail.com>
2022-10-20 10:50:30 -0400

    Create Mark6.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mark6.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/****************************
+ runOpMode():
+ Code inside this method will run exactly once after you press the INIT button.
+ This is where you should put all code for the OpMode.
+ waitForStart():
+ This method pauses the Op-Mode until you press the START button on the driver station.
+ isStarted():
+ returns true if the START button has been pressed, otherwise it returns false.
+ isStopRequested():
+ returns true if the STOP button has been pressed, otherwise it returns false.
+ idle():
+ calls Thread.yield, allowing other threads at the same priority level to run.
+ opModeIsActive():
+ returns isStarted() && !isStopRequested() and calls idle().
+ opModeInInit():
+ returns !isStarted() && !isStopRequested() and does not call idle().
+ *****************************/
+
+@TeleOp(name = "Mark VI.", group="Linear OpMode")
+// @Disabled
+public class Mark6 extends LinearOpMode {
+
+    Servo grabby;
+    DcMotor lift;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Located in the Hardware file and matches with the Drive Hub robot settings
+    private DcMotor frontLeftMotor = null; // assigned 1 in Driver Hub
+    private DcMotor frontRightMotor = null; // assigned 0 in Driver Hub
+    private DcMotor backRightMotor = null; // assigned 2 in Driver Hub
+    private DcMotor backLeftMotor = null; // assigned 3 in Driver Hub
+    BNO055IMU imu;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        grabby = hardwareMap.servo.get("grabby");
+
+        // TODO string to hardware map for lift
+        lift = hardwareMap.get(DcMotorEx.class,"lift");
+
+        // use braking to slow the motor down
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // disables the default velocity control
+        // this does NOT disable the encoder from counting,
+        // but lets us simply send raw motor power.
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        // Set starting position of the grabby claw. 0.5 is open, 0.0 is closed
+        grabby.setPosition(0.5);
+
+        frontLeftMotor = hardwareMap.get(DcMotor.class,"frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class,"frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class,"backRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class,"backLeftMotor");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        /*
+         * Both right side motors should be going in one direction,
+         * and both left side motors going in the opposite direction
+         */
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+
+            if (gamepad1.b) {
+                grabby.setPosition(0.5);
+            }
+            if (gamepad1.a){
+                grabby.setPosition(0);
+            }
+
+            telemetry.update();
+
+            // Drives the robot forward and backwards
+            double y = -gamepad1.left_stick_y; // Uses the left thumbstick for left and right robot movement
+            double x = gamepad1.left_stick_x; //*1.1 to counteract imperfect strafing
+            double rot = gamepad1.right_stick_x; // Uses the right thumbstick to rotate robot movement
+
+            double frontLeftPower = (y + x + rot);
+            double backLeftPower = (y - x + rot);
+            double frontRightPower = (y - x - rot);
+            double backRightPower = (y + x - rot);
+
+            // Send calculated power to wheels
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+
+            }
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "Front L (%.2f), Front R (%.2f)", frontLeftPower, frontRightPower);
+            telemetry.addData("Motors", "Back L (%.2f), Back R (%.2f)", backLeftPower, backRightPower);
+            telemetry.update();
+
+        }
+    }
+} // End of Class
