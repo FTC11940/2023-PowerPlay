23aa2dcb042a80e3a38100e38d0ea2cd3de10b12
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2022-11-08 21:42:30 -0500

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,39 +1,20 @@
-/****
-* Constants file for FTC code base
-***/
+/*
+ * Constants used across the our FTC code base
+ * Naming conventions https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html
+ */
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
 public class Constants {
 
-    /* SetTargetPosition() for the lift preset encoder values. 
-    Values to be determined experimental and set here
-    ADD import static org.firstinspires.ftc.teamcode.Constants.*;
-    to each file reference constants
-    */
 
-    // SetTargetPosition() is the game field zero
     public static final int LIFT_GROUND = 100; // 1" Ground junction height
     public static final int LIFT_LOW = 1200; // 13.5" Low junction height
     public static final int LIFT_MEDIUM = 2000; // 23.5" Medium junction height
     public static final int LIFT_HIGH = 2800; // 33.5" High junction height
     public static final int TOLERANCE = 10;
 
-    // Autonomous constants
-    public static final double DRIVE_SPEED = 0.8;
-    public static final double TURN_SPEED = 0.4;
-
-    // Vuforia constants
-    public static final String VUFORIA_KEY =
-            "AQlm0VH/////AAABmecSieTQsU26rgeA9+8pNJ0JiCO/pfP8X82+Kv4Czexw7qCupZvMaBQROIVF3rj9aoXmjtRDTA1mHJVdaV6hWpfm0jMW8qnLFUmIJQwHIYZ9aM/sQR71rjIx9L5REWzQ9LTYIwLGaW3gwBbaOXpOZ/yi2mt4Q/RIes7idEhwoRkFQh0H+zHxO0iqZdbbUibMlTMif/0g/FqqjBY/abecyaNvfCErZX6jVLlMXPk4cyEeCO6i57ufYKhrOMAHa2BOEgbjvTx52vUeZHh5nORqSa/mJy7ZzE2mrSG4SJ7KEZm1C7Lbnzo6WtMj+F6e1gqISfrynyNJoMrlq9uUHzm8gEbcebYncHx869F+ykNDQTZ7";
-
-    // SetPosition() values for Open and Close on the claw servo
-    public static final double OPEN = 0.3;
-    public static final double CLOSED = 0.0;
-
-    //
+    // Same as above for lift, just different variable names
     public static final int lift_floor = 0;
     public static final int lift_ground = 100; // 1" Ground junction height
     public static final int lift_low = 1200; // 13.5" Low junction height
@@ -41,6 +22,20 @@ public class Constants {
     public static final int lift_high = 2800; // 33.5" High junction height
     public static final int diplomat = 10;
 
+
+    public static final double OPEN = 0.5;
+    public static final double CLOSED = 0.0;
+    public static final double GRABBY_OPEN = 0.5;
+    public static final double GRABBY_CLOSED = 0.0;
+
+    // Autonomous constants
+    public static final double DRIVE_SPEED = 0.6;
+    public static final double TURN_SPEED = 0.45;
+    public static final double HEADING_THRESHOLD = 1.0;
+
+    // Vuforia constants
+    public static final String VUFORIA_KEY =
+            "AQlm0VH/////AAABmecSieTQsU26rgeA9+8pNJ0JiCO/pfP8X82+Kv4Czexw7qCupZvMaBQROIVF3rj9aoXmjtRDTA1mHJVdaV6hWpfm0jMW8qnLFUmIJQwHIYZ9aM/sQR71rjIx9L5REWzQ9LTYIwLGaW3gwBbaOXpOZ/yi2mt4Q/RIes7idEhwoRkFQh0H+zHxO0iqZdbbUibMlTMif/0g/FqqjBY/abecyaNvfCErZX6jVLlMXPk4cyEeCO6i57ufYKhrOMAHa2BOEgbjvTx52vUeZHh5nORqSa/mJy7ZzE2mrSG4SJ7KEZm1C7Lbnzo6WtMj+F6e1gqISfrynyNJoMrlq9uUHzm8gEbcebYncHx869F+ykNDQTZ7";
     /*
      * Specify the source for the Tensor Flow Model.
      * If the TensorFlowLite object model is included in the Robot Controller App as an "asset",
@@ -48,14 +43,11 @@ public class Constants {
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+
+    public static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+
+    // Use this if adding custom signal images
     // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
 
-
-    private static final String[] LABELS = {
-            "1 Bolt",
-            "2 Bulb",
-            "3 Panel"
-    };
 
 }
