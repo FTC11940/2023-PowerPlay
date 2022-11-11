5e19ff99a7332f8c08a7be28262ea355bef4283c
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2022-11-11 06:31:19 -0500

    Revert "Added stack lift constants"
    Co-Authored-By: lvanscoyoc <56980908+lvanscoyoc@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -10,9 +10,6 @@ public class Constants {
     // Preset lift heights using encoder ticks
     public static final int LIFT_FLOOR = 0; // 1" Ground junction height
     public static final int LIFT_GROUND = 100; // 1" Ground junction height
-    public static final int LIFT_TOP_STACK = 300; // for use in Auton
-    public static final int LIFT_THREE_STACK = 232; // for use in Auton
-    public static final int LIFT_TWO_STACK = 166; // for use in Auton
     public static final int LIFT_LOW = 1200; // 13.5" Low junction height
     public static final int LIFT_MEDIUM = 2000; // 23.5" Medium junction height
     public static final int LIFT_HIGH = 2800; // 33.5" High junction height
